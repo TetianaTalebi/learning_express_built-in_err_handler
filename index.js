1d0dfee14368c21aae1b52648a4ff68bbbb835bb
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2024-07-07 00:14:05 -0400

    Add app.listen(8080... in index.js

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 const express = require('express');
 const app = express();
+
+
+// Start a web server and listen to the connections on port 8080
+app.listen(8080, ()=>{
+    console.log('Listening on port 8080!')
+})
