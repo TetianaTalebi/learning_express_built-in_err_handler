f3c7b7506b2019ca41eb9af1f91e67a64d9aebe3
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2024-07-08 22:34:35 -0400

    Add a route app.get /error which callback contains undefined object

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ app.get('/', (req, res) => {
 })
 
 
+app.get('/error', (req, res) =>{
+    chicken.fly();
+})
+
+
 // Start a web server and listen to the connections on port 8080
 app.listen(8080, ()=>{
     console.log('Listening on port 8080!')
