4d46cf7016196a8c17adc4ebd7679edc5a1452a9
Tetiana Talebi <tetiana.talebi1111@gmail.com>
2024-07-08 22:09:02 -0400

    Add a simple Home page route into index.js

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send('Home page!')
+})
+
 
 // Start a web server and listen to the connections on port 8080
 app.listen(8080, ()=>{
